c9adfc1fb18d85ea18af6edbd8cbf5d2d0a0b35b
sitong <sitong@forge.gg>
2018-10-17 14:35:09 -0700

    Allow electron cookies to accept a URI

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ const WebStorageCookieStore = require('tough-cookie-web-storage-store');
 
 const { Cookie } = tough;
 
-// This should resemble a real URI, but have a fake TLD. We don't want to have it so that
-// it's possible to send these cookies to a domain someone could register after the fact, but
-// for Heap, we need a parseable URI because internally we try to determine the right level to set
-// a cookie, rather than having a known set of public suffix domains.
-const FAKE_APP_URI = 'https://yourdomain.heap/';
+function electronCookies(document, uri) {
+  // This should resemble a real URI, but have a fake TLD. We don't want to have it so that
+  // it's possible to send these cookies to a domain someone could register after the fact, but
+  // for Heap, we need a parseable URI because internally we try to determine the right level to set
+  // a cookie, rather than having a known set of public suffix domains.
+  const FAKE_APP_URI = uri || 'https://yourdomain.heap/';
 
-(function(document) {
   const store = new WebStorageCookieStore(localStorage);
 
   const cookiejar = new tough.CookieJar(store);
+
+  if (uri) {
+    cookiejar.rejectPublicSuffixes = false
+  }
+
   Object.defineProperty(document, "cookie", {
     get() {
       return cookiejar.getCookieStringSync(FAKE_APP_URI);
@@ -23,4 +28,8 @@ const FAKE_APP_URI = 'https://yourdomain.heap/';
       cookiejar.setCookieSync(Cookie.parse(cookie, {loose: true}), FAKE_APP_URI);
     }
   });
-})(document);
+}
+
+module.exports = {
+  electronCookies: electronCookies
+}
